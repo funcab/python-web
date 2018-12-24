b58fb70fa3ebd6778e532b2925e1473632f8af40
Angelique He <hexuanr@foxmail.com>
2018-12-24 13:06:18 +0800

    hhh

--- socket/socket-client.py
+++ socket/socket-client.py
@@ -4,3 +4,9 @@ http请求为单向，三次握手后，client可以向server发送数据，serv
 而这个过程只能完成一次，server不能主动多次向client发送数据，需要频繁的三次握手连接
 socket请求为双向，client和server都可以主动多次发送数据
 '''
+import socket
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#绑定IP和端口
+client.connect(('localhost',8000))
+client.send("boddy".encode("utf8"))
+#client.close()
--- socket/socket-server.py
+++ socket/socket-server.py
@@ -10,5 +10,12 @@ server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(('0.0.0.0',8000))
 #开启监听
 server.listen()
-#接收服务器的返回
-sock, addr = server.accept()
+#接收服务器的返回SS
+while True:
+    sock, addr = server.accept()
+    #获取从客户端发送的数据
+    #一次获取1K的数据
+    data = sock.recv(1024)
+    print(data.decode("utf8"))
+socket.close()
+sock.close()
